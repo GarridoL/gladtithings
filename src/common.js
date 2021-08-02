5e77587a6b6dd07a518028927c39275c08bdf132
GarridoL <lalainecabelin@gmail.com>
2021-08-02 13:24:08 +0800

    login and sign up page

---
+++ src/common.js
@@ -0,0 +1,36 @@
+import config from 'src/config.js'
+let APP_EMAIL = 'support@siml.ph'
+export default {
+  sidebarMenu: [
+    {accountType: 'ALL', showOnAdmin: true, accountStatus: 'ALL', description: 'Dashboard', icon: 'fa fa-home', path: 'dashboard', flag: false, subMenu: null},
+    {accountType: 'ADMIN', showOnAdmin: true, accountStatus: 'ADMIN', description: 'Accounts', icon: 'fa fa-users', path: 'accounts', flag: false, subMenu: null}
+  ],
+  menuOff: [
+    {id: 1, users: 'ALL', parent_id: 0, description: 'Dashboard', icon: 'fa fa-tachometer', path: 'dashboard', flag: false, subMenu: null}
+  ],
+  APP_NAME: 'Glad Tithings',
+  APP_NAME_HTML: 'Glad Tithings',
+  APP_EMAIL: APP_EMAIL,
+  APP_SITE: 'https://wearesiml.com',
+  COMPANY: 'Glad Tithings',
+  COMPANY_URL: 'http://wearesiml.ltd',
+  COPYRIGHT: 'Glad Tithings ' + new Date().getFullYear(),
+  USER_TYPE: [{
+    title: 'USER'
+  }],
+  profileMenu: [
+    {
+      title: 'My Profile',
+      icon: 'fa fa-cog',
+      route: '/profile'
+    }],
+  USER_TYPE_SETTING: false,
+  plan: false,
+  broadcastingFlag: true,
+  header: ['status', 'notification'], // 'messenger', '',
+  settingsMenu: [
+    {title: 'Profile', type: 'profile', allowed: ['cellular_number', 'address']},
+    {title: 'Account', type: 'account', allowed: []},
+    {title: 'Business Setting', type: 'merchant', allowed: ['prefix', 'website', 'address', 'schedule', 'information']}
+  ]
+}
