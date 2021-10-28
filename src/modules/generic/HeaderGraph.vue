8515595bc754f46144ef795ac84490a01458db6b
chloe868 <cherrymae.herrera@student.passerellesnumeriques.org>
2021-10-28 19:03:32 +0800

    Updates

--- src/modules/generic/HeaderGraph.vue
+++ src/modules/generic/HeaderGraph.vue
@@ -36,17 +36,41 @@
 			</div>
 		</div>
 		<div class="card">
-			<div class="card-body">
+			<div class="card-body cardCollapsible" style="display:flex; flex-direction: row">
 				<!-- <h5 class="card-title">Card title</h5> -->
-				<p class="card-text">Custom: </p>
+				<p class="card-text paragraphStyling">Custom: </p>
+				<!-- <date-picker
+					class="date"
+					v-model="custom"
+					range
+					:value-type="'YYYY-MM-DD'"
+					:format="'MMM D, YYYY'"
+					:input-class="'form-control'"
+				> -->
+					<!-- :type="'date'" -->
+				<!-- </date-picker> -->
 				<!-- <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p> -->
 			</div>
 		</div>
 	</div>
 </template>
+<script>
+import DatePicker from 'vue2-datepicker'
+import 'vue2-datepicker/index.css'
+export default {
+  data(){
+  },
+  components: {
+    DatePicker
+  }
+}
+</script>
 <style scoped>
 .card{
-	padding: -2%;
+	padding: -3%;
 	text-align: center;
+}
+.card-group{
+	display: flex;
 }
 </style>
