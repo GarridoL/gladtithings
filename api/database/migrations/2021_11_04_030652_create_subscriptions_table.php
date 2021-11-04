d222bee665dd5916da24a6b17f1c984b70d54b22
pathran-phantom1823 <patrick.pathran23@gmail.com>
2021-11-04 11:43:28 +0800

    new migration

---
+++ api/database/migrations/2021_11_04_030652_create_subscriptions_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSubscriptionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subscriptions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('code');
+            $table->bigInteger('account_id');
+            $table->string('plan');
+            $table->double('amount', 8, 2);
+            $table->string('currency');
+            $table->datetime('start_date')->nullable();
+            $table->datetime('end_date')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subscriptions');
+    }
+}
