e6f7d78e04ad72679199a956f9a0ea5b0f294667
GarridoL <lalaine.garrido@student.passerellesnumeriques.org>
2021-11-22 11:33:09 +0800

    updates-events

---
+++ api/app/Event.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends APIModel
+{
+    protected $table = 'events';
+    protected $fillable = ['account_id', 'name', 'description', 'type', 'category', 'location', 'start_date', 'end_date', 'time_zone', 'created_at', 'updated_at', 'deleted_at'];
+}
---
+++ src/modules/generic/TableHeader.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="table-container">
+    <table class="table table-bordered table-responsive" v-if="data !== null">
+      <thead>
+        <tr>
+          <td class="header" v-for="(item, index) in headers" :key="index"><b>{{item}}</b></td>
+        </tr>
+      </thead>
+    </table>
+  </div>
+</template>
+<script>
+export default{
+  props: ['headers']
+}
+</script>
+<style scoped lang="scss">
+@import "~assets/style/colors.scss";
+.header {
+  text-align: center;
+}
+.table-container{
+  height: 50vh;
+  background-color: white;
+  margin-bottom: 10px;
+  margin-top: 50px;
+}
+</style>
