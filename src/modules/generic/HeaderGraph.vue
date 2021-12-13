9b32e2d659b48fe8ec8b31eff307d0b8efe74882
chloe868 <cherrymae.herrera@student.passerellesnumeriques.org>
2021-12-13 15:19:23 +0800

    Updates

--- src/modules/generic/HeaderGraph.vue
+++ src/modules/generic/HeaderGraph.vue
@@ -3,11 +3,11 @@
     <table class="table table-bordered">
           <thead style="text-align: left; margin-top: 3%">
             <tr class="header123">
-              <td class="header" @click="show('yearly')"><b>Yearly</b></td>
-              <td class="header" @click="show('current_year')"><b>This Year</b></td>
-              <td class="header" @click="show('yearly')"><b>Last Month</b></td>
-              <td class="header" @click="show('last_month')"><b>This Month</b></td>
-              <td class="header" @click="show('current_month')"><b>Last 7 Days</b></td>
+              <td class="header" id="yearly" @click="activate('yearly')"><b>Yearly</b></td>
+              <td class="header" id="current_year" @click="activate('current_year')"><b>This Year</b></td>
+              <td class="header" id="last_month" @click="activate('last_month')"><b>Last Month</b></td>
+              <td class="header" id="current_month" @click="activate('current_month')"><b>This Month</b></td>
+              <td class="header" id="last_days" @click="activate('last_days')"><b>Last 7 Days</b></td>
               <td class="header">
                 <b>Custom:</b>
                 <date-picker
@@ -23,7 +23,7 @@
                     <div class="tooltip">Export
                     </div>
                   </div>
-                  <div class="fas fa-print hover">
+                  <div class="fas fa-print hover" @click="print(data)">
                     <div class="tooltip">Print
                     </div>
                   </div>
@@ -34,20 +34,48 @@
   </div> 
 </template>
 <script>
+import pdfFonts from 'pdfmake/build/vfs_fonts'
+import PDFTemplate from 'pdfmake'
+import TemplatePdf from './PdfTemplate.js'
 import DatePicker from 'vue2-datepicker'
 import 'vue2-datepicker/index.css'
 export default {
+  name: 'GraphHeader',
+  props: ['data'],
+  mounted(){
+    const {vfs} = pdfFonts.pdfMake
+    PDFTemplate.vfs = vfs
+  },
   data() {
     return {
-      custom: null
+      PdfTemplate: TemplatePdf,
+      custom: null,
+      tempStyle: null
     }
   },
   components: {
     DatePicker
   },
   methods: {
-    show(e){
-      this.$emit('select', e)
+    print(data){
+      console.log('[data>>>>>>>>>]', data)
+      // this.PdfTemplate.getImage(this.image)
+      // this.PdfTemplate.getData(this.dataRes)
+      // this.PdfTemplate.getDel(this.addonsDel)
+      this.PdfTemplate.getItem(data)
+      this.PdfTemplate.template()
+    },
+    activate(id){
+      if(this.tempStyle === null){
+        this.tempStyle = id
+        document.getElementById(`${id}`).style.backgroundColor = 'white'
+      }else{
+        document.getElementById(`${this.tempStyle}`).style.backgroundColor = '#F6F6F6'
+        document.getElementById(`${id}`).style.backgroundColor = 'white'
+        // let tempStyle = document.querySelector(`#${tempStyle}`)
+        this.tempStyle = id
+        this.$emit('temp', this.tempStyle)
+      }
     }
   }
 }
@@ -59,14 +87,17 @@ $(document).ready(function(){
 @import '~assets/style/colors.scss';
 
 .datetime-picker{
-  width: 35% !important;
+  width: 49% !important;
   margin-left: 2%;
 }
 .header{
 	text-align: center;
+  vertical-align: middle;
+  background-color: #F6F6F6;
 }
 .fa-file-export {
   margin-right: 2px;
+  margin-left: 2%;
 }
 
 .fa-file-export, .fa-print{
