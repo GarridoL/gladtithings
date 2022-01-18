10a59d87523dab4ef83676f690931a0919fbffce
GarridoL <lalaine.garrido@student.passerellesnumeriques.org>
2022-01-18 10:19:54 +0800

    udpates

--- api/app/Http/Controllers/ReactionController.php
+++ api/app/Http/Controllers/ReactionController.php
@@ -12,8 +12,14 @@ class ReactionController extends APIController
 
   public function retrieveWithPayload($column, $clause, $value, $column1, $clause1, $value1){
     $result = Reaction::where($column, $clause, $value)->where($column1, $clause1, $value1)->select('account_id')->get();
+    $final_result = array();
+    $i = 0;
     if(sizeof($result) > 0) {
-      return $result;
+      foreach($result as $key) {
+        array_push($final_result, $result['account_id']);
+        $i++;
+      }
+      return $final_result;
     } else {
       return [];
     }
