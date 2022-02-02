fcfd39bfdfbe200370604ae015eca2c4ed987e2a
GarridoL <lalaine.garrido@student.passerellesnumeriques.org>
2022-02-02 11:18:38 +0800

    udpates

--- api/app/Http/Controllers/EventAttendeesController.php
+++ api/app/Http/Controllers/EventAttendeesController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\EventAttendee;
 use Illuminate\Http\Request;
 
-class ReportController extends APIController
+class EventAttendeesController extends APIController
 {
     function __construct(){
         $this->model = new EventAttendee();
