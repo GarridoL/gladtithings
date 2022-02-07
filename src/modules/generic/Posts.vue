c4e357f2456f9a1e4ba1aeb5fd5d09c8d53dd41f
GarridoL <lalaine.garrido@student.passerellesnumeriques.org>
2022-02-07 13:43:42 +0800

    updates

--- src/modules/generic/Posts.vue
+++ src/modules/generic/Posts.vue
@@ -1,5 +1,25 @@
 <template>
   <div class="containers">
+    <div class="row" style="width: 103%; margin-left: 0px;" v-if="data.shared !== null">
+      <div class="column" style="margin-right: 10px;">
+        <img :src="data.shared.account.profile !== null ? config.BACKEND_URL + data.shared.account.profile.url : require('src/assets/img/test.jpg')" width="40px" height="40px" style="border-radius: 25px; margin-left: 2px;">
+      </div>
+      <div class="column" style=" width: 84%; margin-top: 4px; line-height: 15px;"> 
+        <b>{{data.shared.account.username}}</b>
+        <p style="font-size: 12px;">{{data.shared.created_at_human}}</p>
+      </div>
+      <div class="column" style="padding-right: 8px;">
+        <span class="right-menu-icons">
+        <div class="dropdown">
+          <span class="nav-item"  data-toggle="dropdown" v-on:click="makeActive('dropdown')" >
+          <i class="fas fa-ellipsis-h"></i>
+          <Dropdown :account_id="data.shared.account_id" :id="data.id" :text="data.text"/>
+          </span>
+        </div>
+        </span>
+      </div>
+    </div>
+    <p class="shared-text" v-if="data.shared !== null">{{data.shared.text}}</p>
     <div class="row" style="width: 103%; margin-left: 0px;">
       <div class="column" style="margin-right: 10px;">
         <img :src="data.account.profile && data.account.profile.url ? config.BACKEND_URL + data.account.profile.url : require('src/assets/img/test.jpg')" width="40px" height="40px" style="border-radius: 25px; margin-left: 2px;">
@@ -9,15 +29,15 @@
         <p style="font-size: 12px;">{{data.created_at_human}}</p>
       </div>
       <div class="column" style="padding-right: 8px;">
-      <span class="right-menu-icons">
-      <div class="dropdown" >
-        <span class="nav-item"  data-toggle="dropdown" v-on:click="makeActive('dropdown')" >
-        <i class="fas fa-ellipsis-h"></i>
-        <Dropdown :account_id="data.account_id" :id="data.id" :text="data.text"/>
+        <span class="right-menu-icons">
+        <div class="dropdown" v-if="data.shared === null">
+          <span class="nav-item"  data-toggle="dropdown" v-on:click="makeActive('dropdown')" >
+          <i class="fas fa-ellipsis-h"></i>
+          <Dropdown :account_id="data.account_id" :id="data.id" :text="data.text"/>
+          </span>
+        </div>
         </span>
       </div>
-      </span>
-    </div>
     </div>
     <div style="padding: 10px;" class="body-comment">
       <p style="word-wrap: break-word;">{{data.text}}</p>
@@ -26,8 +46,8 @@
       <span style=" margin-right: 50px; vertical-align: 2px;" @click="react('amen')">{{data.amen.length}}</span>
       <i :class="data.love.includes(user.userID) ? 'fas fa-heart love-true' : 'fas fa-heart love'" @click="react('love')"></i>
       <span style=" margin-right: 50px; vertical-align: 3px;" @click="react('love')">{{data.love.length}}</span>
-      <i class="fas fa-share share"></i>
-      <span style=" margin-right: 50px; vertical-align: 3px;">Share</span>
+      <i class="fas fa-share share" @click="shareModalShow()" ></i>
+      <span style=" margin-right: 50px; vertical-align: 3px;" @click="shareModalShow()">Share</span>
     </div>
     <div style="width: 100%; margin-left: 0px; margin: 10px;" v-if="data.comment_replies && data.comment_replies.length > 0">
       <div style="margin-left: 15px;" v-for="(item, index) in data.comment_replies" :key="index">
@@ -56,6 +76,21 @@
     <div style="width: 100%; margin-left: 0px; margin: 10px;">
       <input type="text" class="inputs" placeholder="Type here" v-on:keyup.enter="reply()" v-model="replyHere">
     </div>
+    <div class="modal fade" id="shareModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLongTitle">Share this post</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true" @click="closeModal()">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <input type="text" class="inputs-share" placeholder="Say something about this post." v-model="status" v-on:keyup.enter="sharePost">
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -70,7 +105,8 @@ export default{
       user: AUTH.user,
       dropdown: 'dropdown-menu',
       config: CONFIG,
-      replyHere: null
+      replyHere: null,
+      status: null
     }
   },
   components: {
@@ -84,6 +120,40 @@ export default{
         this.menuFlag = true
         this.notifFlag = false
       }
+    },
+    shareModalShow() {
+      this.status = null
+      $('#shareModal').modal('show')
+    },
+    sharePost() {
+      if(this.status === null || this.status === '') {
+        return
+      }
+      let parameter = {
+        account_id: this.user.userID,
+        text: this.status,
+        comment_id: this.data.id
+      }
+      $('#loading').css({display: 'block'})
+      this.APIRequest('share_posts/create', parameter).then(response => {
+        $('#loading').css({display: 'none'})
+        if(response.data > 0) {
+          $('#shareModal').modal('hide')
+        }
+        let par = {
+          account_id: this.data.account_id,
+          payload: 'share_post_id',
+          payload_value: response.data,
+          text: this.data.text,
+          to: this.user.userID,
+          from: this.user.userID,
+          route: 'statusStack'
+        }
+        $('#loading').css({display: 'block'})
+        this.APIRequest('comments/create', par).then(res => {
+          $('#loading').css({display: 'none'})
+        })
+      })
     },
     react(react) {
       let list = []
@@ -178,6 +248,24 @@ export default{
   outline: none;
   width: 95%;
   padding: 15px;
+}
+.shared-text{
+  word-wrap: break-word;
+  border-bottom: .5px solid rgb(235, 235, 235);
+  padding: 10px;
+}
+.inputs-share{
+  border-top-style: hidden;
+  border-right-style: hidden;
+  border-left-style: hidden;
+  border-bottom-style: hidden;
+  margin-bottom: 20px;
+  width: 460px;
+  height: 40px;
+  margin-left: -10px;
+}
+input:focus, textarea:focus, select:focus{
+  outline: none;
 }
 .body-comment {
   border-bottom: .5px solid rgb(245, 244, 244);
