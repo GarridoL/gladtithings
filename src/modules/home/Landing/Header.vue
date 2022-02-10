6e72a30c9e9d80e833114e30defc05e2864b0c50
GarridoL <lalaine.garrido@student.passerellesnumeriques.org>
2022-02-10 16:20:34 +0800

    updates

--- src/modules/home/Landing/Header.vue
+++ src/modules/home/Landing/Header.vue
@@ -13,8 +13,7 @@
         </span>
         <ul class="header-primary-menu">
           <li class="nav-item" v-on:click="redirect('/signup')"><a class="nav-link">Register</a></li>
-          <li class="nav-item" v-on:click="redirect('/login')"><a class="nav-link">Login</a></li>
-          <li class="nav-item" v-on:click="scrollTo('#services')"><a class="nav-link">Features</a></li>          
+          <li class="nav-item" v-on:click="redirect('/login')"><a class="nav-link">Login</a></li>       
         </ul>
       </span>
     </div>
