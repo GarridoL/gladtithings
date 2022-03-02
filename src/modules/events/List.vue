c0534fd4b003ba0fd60eb2835c0308d364fdb0eb
GarridoL <lalaine.garrido@student.passerellesnumeriques.org>
2022-03-02 13:39:17 +0800

    updates

--- src/modules/events/List.vue
+++ src/modules/events/List.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <div>
-      <basic-filter 
+      <basic-filter
         v-bind:category="category" 
         :activeCategoryIndex="0"
         :activeSortingIndex="0"
@@ -78,11 +78,13 @@ export default{
         sorting: [{
           title: 'Created Ascending',
           payload: 'created_at',
-          payload_value: 'asc'
+          payload_value: 'asc',
+          input_type: 'date'
         }, {
           title: 'Created Descending',
           payload: 'created_at',
-          payload_value: 'desc'
+          payload_value: 'desc',
+          input_type: 'date'
         }, {
           title: 'Event Name Ascending',
           payload: 'name',
