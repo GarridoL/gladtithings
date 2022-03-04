9aa7d8942287b3cd73b7279c3a685e8a32b875b8
GarridoL <lalaine.garrido@student.passerellesnumeriques.org>
2022-03-04 14:00:47 +0800

    updates

--- api/app/Event.php
+++ api/app/Event.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Event extends APIModel
 {
     protected $table = 'events';
-    protected $fillable = ['account_id', 'name', 'description', 'type', 'category', 'location', 'start_date', 'end_date', 'time_zone', 'created_at', 'updated_at', 'deleted_at'];
+    protected $fillable = ['code', 'account_id', 'name', 'description', 'type', 'category', 'location', 'start_date', 'end_date', 'time_zone', 'created_at', 'updated_at', 'deleted_at'];
 }
